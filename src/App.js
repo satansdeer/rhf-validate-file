f6aa030c08f7a006191a406d8686d1ebc9289ec2
Maksim Ivanov <satansdeer@gmail.com>
2021-11-03 13:25:18 +0000

    Fix the app. The validations now run correctly

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import "./App.css";
 import * as yup from "yup";
 
 const schema = yup.object().shape({
   picture: yup.mixed()
-    .required("You need to provide a file")
-    .test("fileSize", "The file is too large", function(value) {
-      console.log('*',value)
-      return false
-      return value && value[0].size <= 2;
+    .test('required', "You need to provide a file", (value) =>{
+      return value && value.length
+    } )
+    .test("fileSize", "The file is too large", (value, context) => {
+      return value && value[0] && value[0].size <= 200000;
     })
-    .test("type", "We only support jpeg", function(value) {
-      return value && value[0].type === "image/jpeg";
+    .test("type", "We only support jpeg", function (value) {
+      return value && value[0] && value[0].type === "image/jpeg";
     }),
 });
 
 function App() {
-  const { register, handleSubmit, errors, watch } = useForm({
-    validationSchema: schema,
+  const { register, handleSubmit, formState: { errors }, watch } = useForm({
+    resolver: yupResolver(schema),
   });
-
-  console.log(errors);
-  console.log('====',watch("picture")); 
-  console.log('-----',watch()); 
-
-
 
   const onSubmit = (data) => {
     alert(JSON.stringify(data));
   };
-
+  
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="picture">Picture</label>
-      <input id="picture" ref={register} type="file" name="picture" />
+      <input id="picture" {...register("picture")} type="file" />
       {errors.picture && <p>{errors.picture.message}</p>}
       <button>Submit</button>
     </form>
